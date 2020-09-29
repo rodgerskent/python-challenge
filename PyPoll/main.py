c94f5c10b71d44b75627e9eeec75d2671e7e7b75
Kent Rodgers <rodgerskent@outlook.com>
2020-09-29 15:42:19 -0600

    PyPoll Tue PM2 Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,14 +9,48 @@ with open(csvpath) as csvfile:
 
     # Stuff that needs to be set outside the for loop
     votescast = 0
-
+    khancounter = 0
+    correycounter = 0
+    licounter = 0
+    otooleycounter = 0
+    
     for row in csvreader:
         # First Goal ... Total Number of Votes Cast
         votescast = votescast + 1
+        otooleycounter = votescast - khancounter - correycounter - licounter
+        # Second Goal ... Complete list of candidates with votes
+        if str(row[2])== 'Khan':
+            khancounter = khancounter + 1
+        if str(row[2])== 'Correy':
+            correycounter = correycounter + 1
+        if str(row[2])== 'Li':
+            licounter = licounter + 1
+        
+    # Vote percentage calculations    
+    khanpercent = khancounter / votescast
+    correypercent = correycounter / votescast
+    lipercent = licounter / votescast
+    tooleypercent = otooleycounter / votescast
 
-        # Second Goal ... Votes cast by candidate
-        
-        
+    # Winner confirmation logic
+    winnervotes = 0
+    winnername = 'Kent'
+
+    if khancounter > winnervotes:
+        winnervotes = khancounter
+        winnername = 'Khan'
+    if correycounter > winnervotes:
+        winnervotes = correycounter
+        winnername = 'Correy'
+    if licounter > winnervotes:
+        winnervotes = licounter
+        winnername = "Li"
+    if otooleycounter > winnervotes:
+        winnervotes = otooleycounter
+        winnername = 'OTooley'
+    # Really should have logic in here to deal with a tie
+         
+
 
     
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
@@ -26,8 +60,12 @@ with open(csvpath) as csvfile:
     print('Total Votes: ' +str(votescast))
     print('------------------------------')
     print('Candidate Results - UNDER CONSTRUCTION')
+    print(f'Khan: ' + (str(khanpercent))+ '%        ' + str(khancounter) + '   Votes')
+    print(f'Correy: ' + str(correypercent) + '%        ' + str(correycounter)+ '   Votes')
+    print(f'Li: ' + str(lipercent) +'%        ' + str(licounter)+ '   Votes')
+    print(f'OTooley: ' +str(tooleypercent) + '%        ' + str(otooleycounter) + '   Votes')
     print('------------------------------')
-    print('Winner: - UNDER CONSTRUCTION')
+    print('Winner Is: ' + str(winnername) + '  with  ' + str(winnervotes) + '   Votes')
     print(' ')
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~kmr')
 
