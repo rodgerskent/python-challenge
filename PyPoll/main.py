573ddcd0d1c0d48d8b7516f4c37e07e1450d5554
Kent Rodgers <rodgerskent@outlook.com>
2020-09-29 13:26:17 -0600

    PyPoll Tue PM1 Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,34 @@
 #Rodgers PyPoll
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
+
+    # Stuff that needs to be set outside the for loop
+    votescast = 0
+
+    for row in csvreader:
+        # First Goal ... Total Number of Votes Cast
+        votescast = votescast + 1
+
+        # Second Goal ... Votes cast by candidate
+        
+        
+
+    
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print(' ')
+    print('Election Results')
+    print('------------------------------')
+    print('Total Votes: ' +str(votescast))
+    print('------------------------------')
+    print('Candidate Results - UNDER CONSTRUCTION')
+    print('------------------------------')
+    print('Winner: - UNDER CONSTRUCTION')
+    print(' ')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~kmr')
+
 
