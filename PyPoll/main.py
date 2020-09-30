ae6b1e988157d8426686edf5bca86cb789066714
Kent Rodgers <rodgerskent@outlook.com>
2020-09-30 15:00:20 -0600

    PyPoll Wednesday PM Two Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,14 +6,29 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader = next(csvreader)
-
+ 
+    #Build a list of candidates
+    namelist = []
+    for candidatexx in csvreader:
+        if not candidatexx[2] in namelist: 
+            #push candidate from row[2] to namelist
+            namelist.append(candidatexx[2])
+    print('~~~~~~~~~~~~~~~~~~~~~~~')
+    print('Candidate list ...')
+    print(namelist)
+    print('~~~~~~~~~~~~~~~~~~~~~~~')
+    
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
+    
     # Stuff that needs to be set outside the for loop
     votescast = 0
     khancounter = 0
     correycounter = 0
     licounter = 0
     otooleycounter = 0
-    
+
     for row in csvreader:
         # First Goal ... Total Number of Votes Cast
         votescast = votescast + 1
@@ -59,7 +74,7 @@ with open(csvpath) as csvfile:
     print('------------------------------')
     print('Total Votes: ' +str(votescast))
     print('------------------------------')
-    print('Candidate Results as follows')
+    print('Candidate Results as follows:')
     print(f'Khan: ' + str(round((khanpercent*100),1)) + '%   ' + str(khancounter) + '   Votes')
     print(f'Correy: ' + str(round((correypercent*100),1)) + '%   ' + str(correycounter)+ '   Votes')
     print(f'Li: ' + str(round((lipercent*100),1)) +'%        ' + str(licounter)+ '   Votes')
@@ -67,9 +82,22 @@ with open(csvpath) as csvfile:
     print('------------------------------')
     print('Winner Is: ' + str(winnername) + '  with  ' + str(winnervotes) + '   Votes')
     print(' ')
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~kmr')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~kmr~~terminal')
 
-    output=(f'Election Results')
+    output=(
+    f'Election Results\n'
+    f'------------------------------\n'
+    f'Total Votes: {str(votescast)} \n'
+    f'------------------------------\n'
+    f'Candidate Results as follows:\n'
+    f'Khan: {str(round((khanpercent*100),1))} % {str(khancounter)} Votes \n'
+    f'Correy: {str(round((correypercent*100),1))} % {str(correycounter)}   Votes\n'
+    f'Li: {str(round((lipercent*100),1))} %  {str(licounter)} Votes\n'
+    f'OTooley: {str(round((tooleypercent*100),1))} % {str(otooleycounter)} Votes\n'
+    f'------------------------------\n'
+    f'Winner Is: {str(winnername)} with {str(winnervotes)} Votes\n'
+    f' \n'
+    f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~kmr~~textfile')
     
     print(output)
     
@@ -78,3 +106,5 @@ with open(csvpath) as csvfile:
     #Open the output file to be written to as a text file; then write
     with open(output_path, "w") as txt_file:
         txt_file.write(output)
+
+    # kmr PyPoll end
